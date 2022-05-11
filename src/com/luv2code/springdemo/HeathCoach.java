480f68e355fb9418fb801d810d76bfbdf9b9c210
annielatte <yabbbklz1@gmail.com>
2022-05-12 00:25:08 +0900

    Add files via upload
    spring dependency injection - xml configuration

---
+++ src/com/luv2code/springdemo/HeathCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class HeathCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
